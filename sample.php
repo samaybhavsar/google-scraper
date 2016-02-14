ea4f49d55c61a468db2b9b318fb6e8e487661d4f
Samay Bhavsar <samaybhavsar@gmail.com>
2016-02-14 18:57:02 +0530

    Added ReadMe File

--- sample.php
+++ sample.php
@@ -3,6 +3,6 @@ ini_set ( 'max_execution_time', 0);
 include 'GoogleScraper.class.php';
 $obj=new GoogleScraper();
 // Pass your keyword and proxy ip here.
-$arr=$obj->getUrlList('keyword','');
+$arr=$obj->getUrlList('apple','');
 print_r($arr);
 ?>
